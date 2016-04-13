d04b98e3c356fee4827b4ce444f38fc294703d63
Wanilton <ton.94@live.com>
2016-04-13 20:53:16 -0300

    open shift

--- config/env-config.js
+++ config/env-config.js
@@ -3,12 +3,11 @@
 var env = {
   appName: 'personaltask',
   version: 'v1',
-  port: process.env.OPENSHIFT_NODEJS_PORT || 8080,
+  port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
   defaultLocale: 'en',
   defaultMessage: 'unespected-error',
-  ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1', 
+  ip: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
   db: {    uri: (process.env.DB_URI || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1/') + 'personaltask',
-
     credentials: {
       name: process.env.DB_USER || '',
       pass: process.env.DB_PASS || ''
